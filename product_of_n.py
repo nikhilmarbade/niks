75d14b73bbdcadb1ee725e237c9e1ca009ec520b
nikhilmarbade <nikhilmarbade03@gmail.com>
2023-01-17 17:00:07 +0530

    Added Product_of_n

---
+++ product_of_n.py
@@ -0,0 +1,19 @@
+def findMinSum(num):
+    sum = 0
+    i = 2
+    while (i * i <= num):
+        while (num % i == 0):
+            sum += i
+            num /= i
+        i += 1
+    sum += num
+
+    # Return sum of numbers
+    # having minimum product
+    return sum
+
+
+# Driver Code
+num = 12
+a  = findMinSum(num)
+print(a)
